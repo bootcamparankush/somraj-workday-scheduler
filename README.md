97619e54cb89740c5de711b2f92edc04ac46d581
Parankush Work <parankushwork@Parankushs-iMac.local>
2023-02-01 22:10:31 -0500

    checking in cleaned up version of the code and working version of the application

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # somraj-workday-scheduler
 
-This is a simple calendar application that allows a user to save events for each hour of the day. This application runs in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+This is a simple calendar application that allows a user to save events for each hour of the day. In addition this application works as ingle-workday planner that saves text inputs and also color codes by the hour depending on if the planner event is in the past, present or future as indicated by the colors: grey, red and green.
 
+This application runs in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
+Build:
+- In HTML semantic tags have been used to aid with accessibility.
+-The site is built using Bootstrap as as a CSS framework.
+-The use of Bootstrap minimises the need for media queries.
+See Bootstrap documentation for customizing the site with their provided options
+Google Fonts allows for the usage of custom fonts that do not have to be installed on the user's machine
+Font Awesome allows for the usage of custom icons
+jQuery powers the dynamic rendering of the html content based on user input
+moment.js is used to manipulate date values for historic data extraction from APIs
--- script.js
+++ script.js
@@ -6,32 +6,12 @@
 
 // Display today's day and date
 
-var todayDate = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-$("#currentDay").html(todayDate);
+var todaysDate = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+$("#currentDay").html(todaysDate);
 
-// $(function () {
-//   // TODO: Add a listener for click events on the save button. This code should
-//   // use the id in the containing time-block as a key to save the user input in
-//   // local storage. HINT: What does `this` reference in the click listener
-//   // function? How can DOM traversal be used to get the "hour-x" id of the
-//   // time-block containing the button that was clicked? How might the id be
-//   // useful when saving the description in local storage?
-//   //
-//   // TODO: Add code to apply the past, present, or future class to each time
-//   // block by comparing the id to the current hour. HINTS: How can the id
-//   // attribute of each time-block be used to conditionally add or remove the
-//   // past, present, and future classes? How can Day.js be used to get the
-//   // current hour in 24-hour time?
-//   //
-//   // TODO: Add code to get any user input that was saved in localStorage and set
-//   // the values of the corresponding textarea elements. HINT: How can the id
-//   // attribute of each time-block be used to do this?
-//   //
-//   // TODO: Add code to display the current date in the header of the page.
-// });
 
 $(document).ready(function () {
-  // saveBtn click listener 
+  // code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements 
   $(".saveBtn").on("click", function () {
       // Get nearby values of the description in JQuery
       var text = $(this).siblings(".description").val();
@@ -45,11 +25,11 @@ $(document).ready(function () {
       //get current number of hours.
       var timeNow = dayjs().hour();
 
-      // loop over time blocks
+      // code to apply the past, present, or future class to each time block by comparing the id to the current hour
       $(".time-block").each(function () {
           var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-          // To check the time and add the classes for background indicators
+          // check the time and add the classes for background indicators
           if (blockTime < timeNow) {
               $(this).removeClass("future");
               $(this).removeClass("present");
